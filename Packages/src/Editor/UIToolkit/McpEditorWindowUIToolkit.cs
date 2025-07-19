cf28c7225ef83bec3ff36771461f9e63e2625212
hatayama <booqle@gmail.com>
2025-07-20 00:14:21 +0900

    chore: add UI Toolkit directory structure and meta files

---
+++ Packages/src/Editor/UIToolkit/McpEditorWindowUIToolkit.cs
@@ -0,0 +1,15 @@
+/*
+ * This file has been moved to replace the original McpEditorWindow.cs
+ * Please refer to /Packages/src/Editor/UI/McpEditorWindow.cs for the UI Toolkit implementation
+ */
+
+using UnityEngine;
+
+namespace io.github.hatayama.uLoopMCP
+{
+    // Placeholder class - moved to McpEditorWindow.cs
+    public class McpEditorWindowUIToolkitPlaceholder : MonoBehaviour
+    {
+        // This class intentionally left empty
+    }
+}
