bd688d3ed32ccf2bcf7216b927178dcab441ee83
hatayama <booqle@gmail.com>
2025-07-24 23:13:44 +0900

    refactor: remove unnecessary type safety disables in handle-connection-lost-use-case

--- Packages/src/TypeScriptServer~/src/domain/use-cases/handle-connection-lost-use-case.ts
+++ Packages/src/TypeScriptServer~/src/domain/use-cases/handle-connection-lost-use-case.ts
@@ -11,8 +11,6 @@
  * - ServiceLocator (infrastructure/service-locator.ts)
  */
 
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
-
 import { UseCase } from '../base-interfaces.js';
 import { HandleConnectionRequest } from '../models/requests.js';
 import { HandleConnectionResponse } from '../models/responses.js';
