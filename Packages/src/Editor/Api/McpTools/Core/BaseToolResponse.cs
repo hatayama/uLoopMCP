a7732666a1365e39729ffdccd23a844b5e5d64b2
hatayama <booqle@gmail.com>
2025-07-31 12:56:08 +0900

    clean: remove unnecessary StartedAt and EndedAt properties from BaseToolResponse

--- Packages/src/Editor/Api/McpTools/Core/BaseToolResponse.cs
+++ Packages/src/Editor/Api/McpTools/Core/BaseToolResponse.cs
@@ -9,31 +9,17 @@ namespace io.github.hatayama.uLoopMCP
     public abstract class BaseToolResponse
     {
         /// <summary>
-        /// Tool execution start time (UTC)
-        /// </summary>
-        public string StartedAt { get; set; }
-
-        /// <summary>
-        /// Tool execution end time (UTC)
-        /// </summary>
-        public string EndedAt { get; set; }
-
-        /// <summary>
         /// Tool execution duration in milliseconds
         /// </summary>
+#pragma warning disable IDE0051 // Remove unused private members
         public long ExecutionTimeMs { get; set; }
+#pragma warning restore IDE0051
 
         /// <summary>
         /// Set timing information automatically
         /// </summary>
         public void SetTimingInfo(DateTime startTime, DateTime endTime)
         {
-            // Convert UTC to local time for display
-            DateTime localStartTime = startTime.ToLocalTime();
-            DateTime localEndTime = endTime.ToLocalTime();
-            
-            StartedAt = localStartTime.ToString("yyyy-MM-ddTHH:mm:ss.fff");
-            EndedAt = localEndTime.ToString("yyyy-MM-ddTHH:mm:ss.fff");
             ExecutionTimeMs = (long)(endTime - startTime).TotalMilliseconds;
         }
     }
