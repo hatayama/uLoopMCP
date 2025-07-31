2e319cccbe950f8a4ec0de9a946c44c1efa980e0
Masamichi Hatayama <booqle@gmail.com>
2025-07-31 13:10:24 +0900

    fix: remove unnecessary timing properties from BaseToolResponse (#274)

--- Packages/src/Editor/Api/McpTools/Core/BaseToolResponse.cs
+++ Packages/src/Editor/Api/McpTools/Core/BaseToolResponse.cs
@@ -9,16 +9,6 @@ namespace io.github.hatayama.uLoopMCP
     public abstract class BaseToolResponse
     {
         /// <summary>
-        /// Tool execution start time (UTC)
-        /// </summary>
-        public string StartedAt { get; set; }
-
-        /// <summary>
-        /// Tool execution end time (UTC)
-        /// </summary>
-        public string EndedAt { get; set; }
-
-        /// <summary>
         /// Tool execution duration in milliseconds
         /// </summary>
         public long ExecutionTimeMs { get; set; }
@@ -28,12 +18,6 @@ namespace io.github.hatayama.uLoopMCP
         /// </summary>
         public void SetTimingInfo(DateTime startTime, DateTime endTime)
         {
-            // Convert UTC to local time for display
-            DateTime localStartTime = startTime.ToLocalTime();
-            DateTime localEndTime = endTime.ToLocalTime();
-            
-            StartedAt = localStartTime.ToString("yyyy-MM-ddTHH:mm:ss.fff");
-            EndedAt = localEndTime.ToString("yyyy-MM-ddTHH:mm:ss.fff");
             ExecutionTimeMs = (long)(endTime - startTime).TotalMilliseconds;
         }
     }
