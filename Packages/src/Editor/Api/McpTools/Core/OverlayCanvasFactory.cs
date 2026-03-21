36dd182324edfe6f5327b272dc63b51f44d2ed25
hatayama <booqle@gmail.com>
2026-03-22 01:16:53 +0900

    Only destroy factory-owned (DontSave) InputVisualizationCanvas instances
    Scene-placed or test canvases without DontSave flag should not be
    destroyed by the factory's lifecycle management. Filter by
    HideFlags.DontSave in both EnsureExists reclaim and PlayMode exit.

--- Packages/src/Editor/Api/McpTools/Core/OverlayCanvasFactory.cs
+++ Packages/src/Editor/Api/McpTools/Core/OverlayCanvasFactory.cs
@@ -37,13 +37,24 @@ namespace io.github.hatayama.uLoopMCP
             // Domain Reload resets _instance but DontSave objects survive; reclaim one and destroy duplicates
             InputVisualizationCanvas[] existing =
                 Object.FindObjectsByType<InputVisualizationCanvas>(FindObjectsSortMode.None);
-            if (existing.Length > 0)
+            for (int i = 0; i < existing.Length; i++)
             {
-                _instance = existing[0];
-                for (int i = 1; i < existing.Length; i++)
+                if ((existing[i].gameObject.hideFlags & HideFlags.DontSave) == 0)
+                {
+                    continue;
+                }
+
+                if (_instance == null)
+                {
+                    _instance = existing[i];
+                }
+                else
                 {
                     Object.DestroyImmediate(existing[i].gameObject);
                 }
+            }
+            if (_instance != null)
+            {
                 return;
             }
 
@@ -67,7 +78,10 @@ namespace io.github.hatayama.uLoopMCP
                 Object.FindObjectsByType<InputVisualizationCanvas>(FindObjectsSortMode.None);
             for (int i = 0; i < canvases.Length; i++)
             {
-                Object.DestroyImmediate(canvases[i].gameObject);
+                if ((canvases[i].gameObject.hideFlags & HideFlags.DontSave) != 0)
+                {
+                    Object.DestroyImmediate(canvases[i].gameObject);
+                }
             }
             _instance = null;
         }
