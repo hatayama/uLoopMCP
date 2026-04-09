8832050f7f4d9b94072ee33be2be0aeba90edf44
Masamichi Hatayama <booqle@gmail.com>
2026-04-10 00:19:00 +0900

    fix: require skills directories for auto-install detection (#912)

--- Assets/Tests/Editor/ToolSkillSynchronizerTests.cs
+++ Assets/Tests/Editor/ToolSkillSynchronizerTests.cs
@@ -8,8 +8,11 @@ namespace io.github.hatayama.uLoopMCP
     [TestFixture]
     public class ToolSkillSynchronizerTests
     {
+        private const string SkillsDirName = "skills";
+
         private string _projectRoot;
         private string[] _nonExistentDirsBefore;
+        private string[] _temporaryRoots;
 
         [SetUp]
         public void SetUp()
@@ -19,6 +22,7 @@ namespace io.github.hatayama.uLoopMCP
             _nonExistentDirsBefore = ToolSkillSynchronizer.SkillTargetDirs
                 .Where(dir => !Directory.Exists(Path.Combine(_projectRoot, dir)))
                 .ToArray();
+            _temporaryRoots = new string[0];
         }
 
         [TearDown]
@@ -31,7 +35,7 @@ namespace io.github.hatayama.uLoopMCP
                 if (Directory.Exists(fullPath))
                 {
                     // Only delete if it was created by this test (didn't exist before)
-                    string skillsPath = Path.Combine(fullPath, "skills");
+                    string skillsPath = Path.Combine(fullPath, SkillsDirName);
                     if (Directory.Exists(skillsPath))
                     {
                         Directory.Delete(skillsPath, true);
@@ -41,6 +45,14 @@ namespace io.github.hatayama.uLoopMCP
                     {
                         Directory.Delete(fullPath);
                     }
+                }
+            }
+
+            foreach (string temporaryRoot in _temporaryRoots)
+            {
+                if (Directory.Exists(temporaryRoot))
+                {
+                    Directory.Delete(temporaryRoot, true);
                 }
             }
         }
@@ -61,6 +73,54 @@ namespace io.github.hatayama.uLoopMCP
                 string fullPath = Path.Combine(_projectRoot, dir);
                 Assert.IsFalse(Directory.Exists(fullPath),
                     $"Directory '{dir}' should not be created by InstallSkillFiles when '{dir}' did not exist");
+            }
+        }
+
+        [Test]
+        public void DetectTargets_DoesNotIncludeTargetsWithOnlyParentDirectory()
+        {
+            // Arrange
+            string temporaryRoot = CreateTemporaryProjectRoot();
+            foreach (string dir in ToolSkillSynchronizer.SkillTargetDirs)
+            {
+                Directory.CreateDirectory(Path.Combine(temporaryRoot, dir));
+            }
+
+            // Act
+            string[] detectedTargetDirs = ToolSkillSynchronizer.DetectTargets(temporaryRoot, requireSkillsDirectory: true)
+                .Select(target => target.DirName)
+                .ToArray();
+
+            // Assert
+            foreach (string dir in ToolSkillSynchronizer.SkillTargetDirs)
+            {
+                Assert.IsFalse(detectedTargetDirs.Contains(dir),
+                    $"Target '{dir}' should not be detected when only the parent directory exists");
+            }
+        }
+
+        [Test]
+        public void DetectTargets_IncludesTargetsWhenSkillsDirectoryExists()
+        {
+            // Arrange
+            string temporaryRoot = CreateTemporaryProjectRoot();
+            foreach (string dir in ToolSkillSynchronizer.SkillTargetDirs)
+            {
+                Directory.CreateDirectory(Path.Combine(temporaryRoot, dir, SkillsDirName));
+            }
+
+            // Act
+            ToolSkillSynchronizer.SkillTargetInfo[] detectedTargets = ToolSkillSynchronizer.DetectTargets(temporaryRoot, requireSkillsDirectory: true)
+                .ToArray();
+
+            // Assert
+            Assert.AreEqual(ToolSkillSynchronizer.SkillTargetDirs.Length, detectedTargets.Length,
+                "Targets with a skills directory should be detected");
+
+            foreach (ToolSkillSynchronizer.SkillTargetInfo target in detectedTargets)
+            {
+                Assert.IsFalse(target.HasExistingSkills,
+                    $"Target '{target.DirName}' should not be treated as already installed when skills directory is empty");
             }
         }
 
@@ -98,6 +158,17 @@ namespace io.github.hatayama.uLoopMCP
                 Assert.IsFalse(Directory.Exists(fullPath),
                     $"Directory '{dir}' should not be created by IsSkillInstalled");
             }
+        }
+
+        private string CreateTemporaryProjectRoot()
+        {
+            string temporaryRoot = Path.Combine(
+                Path.GetTempPath(),
+                "ToolSkillSynchronizerTests",
+                System.Guid.NewGuid().ToString("N"));
+            Directory.CreateDirectory(temporaryRoot);
+            _temporaryRoots = _temporaryRoots.Append(temporaryRoot).ToArray();
+            return temporaryRoot;
         }
     }
 }
