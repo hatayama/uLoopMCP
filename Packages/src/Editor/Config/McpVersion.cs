589c30166470c08001a5c926bea7fd2b5b6be9cd
hatayama <booqle@gmail.com>
2026-04-30 01:25:05 +0900

    Use beta prerelease versions for v3
    Keep the v3 beta branch on 3.0.0-beta.0 instead of a stable 3.0.0 baseline, configure Release Please for beta prereleases, and publish npm prerelease builds with the beta dist-tag so they do not become latest by accident.

--- Packages/src/Editor/Config/McpVersion.cs
+++ Packages/src/Editor/Config/McpVersion.cs
@@ -2,6 +2,6 @@ namespace io.github.hatayama.uLoopMCP
 {
     public static class McpVersion
     {
-        public const string VERSION = "3.0.0"; // x-release-please-version
+        public const string VERSION = "3.0.0-beta.0"; // x-release-please-version
     }
 }
