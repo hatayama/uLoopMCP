355cfe4e179a476026c441b2ac6d1e43769695f4
hatayama <booqle@gmail.com>
2026-05-07 20:22:40 +0900

    Remove redundant registration lock
    UnityCliLoopApplicationRegistration is driven by the composition-root Editor startup path, so a thread lock only obscured the single-registration guard.

--- Packages/src/Editor/CompositionRoot/UnityCliLoopApplicationRegistration.cs
+++ Packages/src/Editor/CompositionRoot/UnityCliLoopApplicationRegistration.cs
@@ -2,44 +2,40 @@ namespace io.github.hatayama.UnityCliLoop
 {
     public static class UnityCliLoopApplicationRegistration
     {
-        private static readonly object SyncRoot = new object();
         private static bool IsRegistered;
 
         internal static void EnsureRegistered()
         {
-            lock (SyncRoot)
+            if (IsRegistered)
             {
-                if (IsRegistered)
-                {
-                    return;
-                }
-
-                ToolSettingsRepository toolSettingsRepository = new();
-                UnityCliLoopEditorSettingsRepository editorSettingsRepository = new();
-                ULoopSettingsRepository uLoopSettingsRepository = new();
-                ToolSettings.RegisterService(toolSettingsRepository);
-                UnityCliLoopEditorSettings.RegisterService(editorSettingsRepository);
-                ULoopSettings.RegisterService(uLoopSettingsRepository);
-                MainThreadSwitcher.RegisterService(new EditorMainThreadDispatcher());
-                CompilationLockService.RegisterService(new CompilationLockFileService());
-                DomainReloadDetectionService.RegisterService(new DomainReloadDetectionFileService());
-                UnityCliLoopToolRegistrar.RegisterService(new UnityCliLoopToolRegistrarService(
-                    new SkillInstallLayoutInternalToolNameProvider()));
-                SkillSetupApplicationFacade.RegisterService(new SkillSetupApplicationService(
-                    new ToolSkillSetupService()));
-                CliSetupApplicationFacade.RegisterService(new CliSetupApplicationService(
-                    new CliInstallationDetector(),
-                    new ProjectLocalCliInstallerService(),
-                    new NativeCliInstallerService()));
-                UnityCliLoopBridgeServerInstanceFactory serverFactory = new();
-                UnityCliLoopServerLifecycleRegistryService lifecycleRegistry = new();
-                lifecycleRegistry.RegisterSource(serverFactory);
-                UnityCliLoopServerControllerService controllerService = new(serverFactory, lifecycleRegistry);
-                UnityCliLoopServerApplicationService applicationService = new(controllerService);
-                UnityCliLoopServerApplicationFacade.RegisterService(applicationService);
-                controllerService.InitializeForEditorStartup();
-                IsRegistered = true;
+                return;
             }
+
+            ToolSettingsRepository toolSettingsRepository = new();
+            UnityCliLoopEditorSettingsRepository editorSettingsRepository = new();
+            ULoopSettingsRepository uLoopSettingsRepository = new();
+            ToolSettings.RegisterService(toolSettingsRepository);
+            UnityCliLoopEditorSettings.RegisterService(editorSettingsRepository);
+            ULoopSettings.RegisterService(uLoopSettingsRepository);
+            MainThreadSwitcher.RegisterService(new EditorMainThreadDispatcher());
+            CompilationLockService.RegisterService(new CompilationLockFileService());
+            DomainReloadDetectionService.RegisterService(new DomainReloadDetectionFileService());
+            UnityCliLoopToolRegistrar.RegisterService(new UnityCliLoopToolRegistrarService(
+                new SkillInstallLayoutInternalToolNameProvider()));
+            SkillSetupApplicationFacade.RegisterService(new SkillSetupApplicationService(
+                new ToolSkillSetupService()));
+            CliSetupApplicationFacade.RegisterService(new CliSetupApplicationService(
+                new CliInstallationDetector(),
+                new ProjectLocalCliInstallerService(),
+                new NativeCliInstallerService()));
+            UnityCliLoopBridgeServerInstanceFactory serverFactory = new();
+            UnityCliLoopServerLifecycleRegistryService lifecycleRegistry = new();
+            lifecycleRegistry.RegisterSource(serverFactory);
+            UnityCliLoopServerControllerService controllerService = new(serverFactory, lifecycleRegistry);
+            UnityCliLoopServerApplicationService applicationService = new(controllerService);
+            UnityCliLoopServerApplicationFacade.RegisterService(applicationService);
+            controllerService.InitializeForEditorStartup();
+            IsRegistered = true;
         }
     }
 }
